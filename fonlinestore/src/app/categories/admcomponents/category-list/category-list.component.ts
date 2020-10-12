0563dcbcec5e88f6804fc01d092eb170bd9f5a76
Lavinia12 <gabrielalaviniay@gmail.com>
2020-10-12 17:22:25 +0300

    no message

---
+++ fonlinestore/src/app/categories/admcomponents/category-list/category-list.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { CategoryService } from '../../service/category.service';
+import { Category } from '../../model/category';
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.css']
+})
+export class CategoryListComponent implements OnInit {
+  category: Category[];
+  closeResult = '';
+  searchValue = '';
+  constructor(private categoryservice: CategoryService,
+              private route: ActivatedRoute,
+              private router: Router,
+              private modalService: NgbModal) { }
+
+  ngOnInit(): void {
+    this.getAll()
+  }
+// tslint:disable-next-line:typedef
+  getAll() {
+    this.categoryservice.findAll().subscribe(data => {
+      this.category = [];
+      this.category = data;
+    });
+  }
+  // tslint:disable-next-line:typedef
+  add(){
+    this.router.navigate(['addCategory']);
+  }
+  // tslint:disable-next-line:typedef
+  editContinent(id: number) {
+    this.router.navigate(['editCategory/' + id]);
+  }
+  // tslint:disable-next-line:typedef
+  delete(id: number) {
+    this.categoryservice.delete(id).subscribe(data => {
+      this.getAll();
+    });
+  }
+  // tslint:disable-next-line:typedef
+  open(content, id) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+      this.delete(id);
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+}
