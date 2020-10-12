f6f5d3be70e29556cc14f5fd1d59c62311034b31
Lavinia12 <gabrielalaviniay@gmail.com>
2020-10-12 17:26:09 +0300

    no message

--- fonlinestore/src/app/app.module.ts
+++ fonlinestore/src/app/app.module.ts
@@ -1,24 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ProductListComponent } from './products/admcomponents/product-list/product-list.component';
-import { ProductAddComponent } from './products/admcomponents/product-add/product-add.component';
-import { ProductEditComponent } from './products/admcomponents/product-edit/product-edit.component';
+import { CategoryListComponent } from './categories/admcomponents/category-list/category-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {NgMultiSelectDropDownModule} from 'ng-multiselect-dropdown';
+import {NgSelectModule} from '@ng-select/ng-select';
+import {Ng2SearchPipeModule} from 'ng2-search-filter';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { CarouselModule, WavesModule } from 'angular-bootstrap-md';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddCategoryComponent } from './categories/admcomponents/add-category/add-category.component';
 
+
+// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ProductAddComponent,
-    ProductEditComponent
+    CategoryListComponent,
+    AddCategoryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MDBBootstrapModule.forRoot(),
+    NgMultiSelectDropDownModule.forRoot(),
+    NgSelectModule,
+    BrowserModule, Ng2SearchPipeModule,
+    BrowserModule, NgxPaginationModule, BrowserAnimationsModule,
+    CarouselModule, WavesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
