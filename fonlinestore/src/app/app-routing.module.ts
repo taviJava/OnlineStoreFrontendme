bfb5e59805b33c94b565e4e243e3db41210ad000
Lavinia12 <gabrielalaviniay@gmail.com>
2020-10-12 17:44:49 +0300

    no message

--- fonlinestore/src/app/app-routing.module.ts
+++ fonlinestore/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './products/admcomponents/product-list/product-list.component';
+import { ProductAddComponent } from './products/admcomponents/product-add/product-add.component';
+import { ProductEditComponent } from './products/admcomponents/product-edit/product-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'product', component: ProductListComponent},
+  {path: 'addProduct', component: ProductAddComponent},
+  {path: 'editProduct', component: ProductEditComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
